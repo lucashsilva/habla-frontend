01c01c58848e7247da772385a5ec0c66a98d8a67
lucashsilva <lucashenrique125@gmail.com>
2018-10-31 16:58:18 -0300

    Simple login page

--- src/pages/login/login.tsx
+++ src/pages/login/login.tsx
@@ -3,18 +3,29 @@ import './login.css';
 import * as firebase from 'firebase';
 
 class LoginPage extends React.Component<any, any> {
+  constructor(props) {
+    super(props);
+
+    this.state = { credentials: { email: '', password: '' }};
+  }
 
   public render() {
     return (
-      <div>
-          <a onClick={this.login}>Sign in</a>
+      <div className="login-box">
+        <input className="text-input" placeholder="Email" name="email" type="text" value={this.state.credentials.email} onChange={this.handleInputChange}></input>
+        <input className="text-input" placeholder="Password" name="password" type="password" value={this.state.credentials.password} onChange={this.handleInputChange}></input>
+        <button className="login-button" onClick={this.login} disabled={!(this.state.credentials.email && this.state.credentials.password)}>Send</button>
       </div>
     );
   }
 
+  handleInputChange = (event) => {
+    this.setState({ credentials: { ...this.state.credentials, [event.target.name]: event.target.value }});
+  };
+
   private login = async() => {
-    await firebase.auth().signInWithEmailAndPassword("lucashenrique125@gmail.com", "12345678");
-  }
+    await firebase.auth().signInWithEmailAndPassword(this.state.credentials.email, this.state.credentials.password);
+  };
 }
 
 export default LoginPage;
