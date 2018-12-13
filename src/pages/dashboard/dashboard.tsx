a33b9a239d494a9a9f4e87319a191cbebef9153d
Lucas Silva <lucashenrique125@gmail.com>
2018-12-13 20:24:48 -0300

    Added redirect from / to /timeline and made profile image rounded

--- src/pages/dashboard/dashboard.tsx
+++ src/pages/dashboard/dashboard.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import './dashboard.css';
 import TimelineComponent from '../../components/timeline/timeline';
 import { Menu, Icon, Grid, Image } from 'semantic-ui-react';
-import { BrowserRouter as Router, Route, NavLink } from "react-router-dom";
+import { BrowserRouter as Router, Route, NavLink, Redirect } from "react-router-dom";
 import ChannelListComponent from '../../components/channel-list/channel-list';
 
 class DashboardPage extends React.Component<any, any> {
@@ -16,7 +16,7 @@ class DashboardPage extends React.Component<any, any> {
       <Router>
         <Grid padded centered>
           <Grid.Column widescreen={2} largeScreen={3} computer={4} tablet={5}>
-            <Image src={this.props.userProfile.photoURL || 'https://react.semantic-ui.com/images/avatar/large/molly.png'}/>
+            <Image rounded src={this.props.userProfile.photoURL || 'https://react.semantic-ui.com/images/avatar/large/molly.png'}/>
             <Menu vertical fluid>
               <Menu.Item as={NavLink} to="/timeline">
                 <Icon name='home' />
@@ -29,6 +29,7 @@ class DashboardPage extends React.Component<any, any> {
             </Menu>
           </Grid.Column>
           <Grid.Column widescreen={8} largeScreen={7} computer={8}>
+            <Redirect exact from="/" to="/timeline" />
             <Route path="/timeline" component={TimelineComponent}/>
             <Route path="/channels" component={ChannelListComponent}/>
             <Route children path="/channel/:channelId" component={TimelineComponent}/>
